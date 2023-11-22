5e6ef35bbb56c73058de93cab86dca4073f1e5d4
rhys2985 <rhys2985@163.com>
2023-11-22 16:46:58 +0800

    调试5

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ from transformers.generation.utils import GenerationConfig
 
 
 # 使用marshmallow作序列化和参数校验
-# blueprint = Blueprint(name="Chat", import_name=__name__, url_prefix="/v1/chat")  # 声明蓝图
 class ChatDeltaSchema(Schema):
     role = fields.Str()
     content = fields.Str()
@@ -73,8 +72,6 @@ class ChatCompletionSchema(Schema):
 def init_env() -> None:
     system("mkdir /tmp/dataset")
     system("unzip /dataset/Baichuan2-7B-Chat.zip -d /tmp/dataset")
-    # system("chmod +x frpc/frpc")  # noqa
-    # system("nohup ./frpc/frpc -c frpc/frpc.ini &")  # noqa
     return
 
 
@@ -96,24 +93,6 @@ def init_model():
     return model, tokenizer
 
 
-# def init_app() -> Tuple[Flask, Blueprint]:
-#     """创建接口服务"""
-#     app = Flask(__name__)  # 声明主服务
-#     CORS(app=app)  # 允许跨域
-#
-#     app.register_blueprint(blueprint=blueprint)  # 注册蓝图
-#
-#     @app.after_request
-#     def after_request(resp: Response) -> Response:
-#         """请求后处理"""
-#         if torch.backends.mps.is_available():  # noqa
-#             torch.mps.empty_cache()  # noqa
-#         return resp
-#
-#     return app, blueprint
-
-# init_env()
-# my_model, my_tokenizer = init_model()
 
 
 def sse(line: Union[str, Dict]) -> str:
@@ -121,45 +100,14 @@ def sse(line: Union[str, Dict]) -> str:
     return "data: {}\n\n".format(dumps(obj=line, ensure_ascii=False) if isinstance(line, dict) else line)
 
 
-# @stream_with_context
-# def stream_chat_generate(messages):
-#     """Chat流式"""
-#     delta = ChatDeltaSchema().dump({"role": "assistant"})
-#     choice = ChatCompletionChunkChoiceSchema().dump({"index": 0, "delta": delta, "finish_reason": None})
-#     yield sse(line=ChatCompletionChunkSchema().dump({"model": "baichuan2-7b-chat", "choices": [choice]}))  # noqa
-#     position = 0
-#     for response in my_model.chat(my_tokenizer, messages, stream=True):
-#         content = response[position:]
-#         if not content:
-#             continue
-#         if torch.backends.mps.is_available():  # noqa
-#             torch.mps.empty_cache()  # noqa
-#         delta = ChatDeltaSchema().dump({"content": content})
-#         choice = ChatCompletionChunkChoiceSchema().dump({"index": 0, "delta": delta, "finish_reason": None})
-#         yield sse(line=ChatCompletionChunkSchema().dump({"model": "baichuan2-7b-chat", "choices": [choice]}))  # noqa
-#         position = len(response)
-#     choice = ChatCompletionChunkChoiceSchema().dump({"index": 0, "delta": {}, "finish_reason": "stop"})
-#     yield sse(line=ChatCompletionChunkSchema().dump({"model": "baichuan2-7b-chat", "choices": [choice]}))  # noqa
-#     yield sse(line="[DONE]")
 
-
-# @blueprint.route("/completions", methods=["POST"])
 def create_chat_completion():
     """Chat接口"""
     return 213
-    # chat_dict = CreateChatCompletionSchema().load(request.json)
-    # # if chat_dict["stream"]:
-    # #     # 切换到流式
-    # #     return current_app.response_class(response=stream_chat_generate(chat_dict["messages"]), mimetype="text/event-stream")
-    # response = my_model.chat(my_tokenizer, chat_dict["messages"])
-    # message = ChatMessageSchema().dump({"role": "assistant", "content": response})
-    # choice = ChatCompletionChoiceSchema().dump({"index": 0, "message": message, "finish_reason": "stop"})
-    # return ChatCompletionSchema().dump({"model": "baichuan2-7b-chat", "choices": [choice]})  # noqa
+
 
 
 if __name__ == "__main__":
-    # my_app, _ = init_app()  # noqa
-    # my_app.run(host="0.0.0.0", port=8262, debug=False)
     app = FastAPI()
     demo = gr.Interface(
         fn=create_chat_completion,
