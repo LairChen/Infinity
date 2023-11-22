29011e756087f58512948d5c36998ce2e6f029a5
rhys2985 <rhys2985@163.com>
2023-11-22 15:31:26 +0800

    调试1

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from json import dumps
-# 使用marshmallow作序列化和参数校验
-# blueprint = Blueprint(name="Chat", import_name=__name__, url_prefix="/v1/chat")  # 声明蓝图
 from logging import INFO, FileHandler, basicConfig, info
 from os import system, getenv
 from time import time
@@ -23,26 +21,28 @@ basicConfig(level=INFO,  # noqa
             handlers=[FileHandler(filename="/result/running.log", mode="w", encoding="utf-8")])
 
 
-class ChatMessageSchema(Schema):
-    role = fields.Str(required=True)
-    content = fields.Str(required=True)
-
-
+# 使用marshmallow作序列化和参数校验
+# blueprint = Blueprint(name="Chat", import_name=__name__, url_prefix="/v1/chat")  # 声明蓝图
 class ChatDeltaSchema(Schema):
     role = fields.Str()
     content = fields.Str()
+
+
+class ChatMessageSchema(Schema):
+    role = fields.Str(required=True)
+    content = fields.Str(required=True)
 
 
 class CreateChatCompletionSchema(Schema):
     model = fields.Str(required=True, metadata={"example": "baichuan2-7b-chat"})  # noqa
     messages = fields.List(fields.Nested(nested=ChatMessageSchema), required=True)  # noqa
     max_tokens = fields.Int(load_default=None)
+    n = fields.Int(load_default=1)
     temperature = fields.Float(load_default=1.0)
     top_p = fields.Float(load_default=1.0)
-    n = fields.Int(load_default=1)
-    stream = fields.Bool(load_default=False)
-    presence_penalty = fields.Float(load_default=0.0)
     frequency_penalty = fields.Float(load_default=0.0)
+    presence_penalty = fields.Float(load_default=0.0)
+    stream = fields.Bool(load_default=False)
 
 
 class ChatCompletionChunkChoiceSchema(Schema):
@@ -55,10 +55,10 @@ class ChatCompletionChunkChoiceSchema(Schema):
 
 class ChatCompletionChunkSchema(Schema):
     id = fields.Str(dump_default=lambda: uuid4().hex)
-    object = fields.Constant(constant="chat.completion.chunk")
-    created = fields.Int(dump_default=lambda: int(time()))
     model = fields.Str(metadata={"example": "baichuan2-7b-chat"})  # noqa
     choices = fields.List(fields.Nested(nested=ChatCompletionChunkChoiceSchema))  # noqa
+    object = fields.Constant(constant="chat.completion.chunk")
+    created = fields.Int(dump_default=lambda: int(time()))
 
 
 class ChatCompletionChoiceSchema(Schema):
@@ -71,10 +71,10 @@ class ChatCompletionChoiceSchema(Schema):
 
 class ChatCompletionSchema(Schema):
     id = fields.Str(dump_default=lambda: uuid4().hex)
-    object = fields.Constant(constant="chat.completion")
-    created = fields.Int(dump_default=lambda: int(time()))
     model = fields.Str(metadata={"example": "baichuan2-7b-chat"})  # noqa
     choices = fields.List(fields.Nested(nested=ChatCompletionChoiceSchema))  # noqa
+    object = fields.Constant(constant="chat.completion")
+    created = fields.Int(dump_default=lambda: int(time()))
 
 
 def init_env() -> None:
@@ -120,9 +120,9 @@ def init_model():
 #     return app, blueprint
 
 info(1111)
-init_env()
+# init_env()
 info(2222)
-my_model, my_tokenizer = init_model()
+# my_model, my_tokenizer = init_model()
 
 info(3333)
 
@@ -132,39 +132,40 @@ def sse(line: Union[str, Dict]) -> str:
     return "data: {}\n\n".format(dumps(obj=line, ensure_ascii=False) if isinstance(line, dict) else line)
 
 
-@stream_with_context
-def stream_chat_generate(messages):
-    """Chat流式"""
-    delta = ChatDeltaSchema().dump({"role": "assistant"})
-    choice = ChatCompletionChunkChoiceSchema().dump({"index": 0, "delta": delta, "finish_reason": None})
-    yield sse(line=ChatCompletionChunkSchema().dump({"model": "baichuan2-7b-chat", "choices": [choice]}))  # noqa
-    position = 0
-    for response in my_model.chat(my_tokenizer, messages, stream=True):
-        content = response[position:]
-        if not content:
-            continue
-        if torch.backends.mps.is_available():  # noqa
-            torch.mps.empty_cache()  # noqa
-        delta = ChatDeltaSchema().dump({"content": content})
-        choice = ChatCompletionChunkChoiceSchema().dump({"index": 0, "delta": delta, "finish_reason": None})
-        yield sse(line=ChatCompletionChunkSchema().dump({"model": "baichuan2-7b-chat", "choices": [choice]}))  # noqa
-        position = len(response)
-    choice = ChatCompletionChunkChoiceSchema().dump({"index": 0, "delta": {}, "finish_reason": "stop"})
-    yield sse(line=ChatCompletionChunkSchema().dump({"model": "baichuan2-7b-chat", "choices": [choice]}))  # noqa
-    yield sse(line="[DONE]")
+# @stream_with_context
+# def stream_chat_generate(messages):
+#     """Chat流式"""
+#     delta = ChatDeltaSchema().dump({"role": "assistant"})
+#     choice = ChatCompletionChunkChoiceSchema().dump({"index": 0, "delta": delta, "finish_reason": None})
+#     yield sse(line=ChatCompletionChunkSchema().dump({"model": "baichuan2-7b-chat", "choices": [choice]}))  # noqa
+#     position = 0
+#     for response in my_model.chat(my_tokenizer, messages, stream=True):
+#         content = response[position:]
+#         if not content:
+#             continue
+#         if torch.backends.mps.is_available():  # noqa
+#             torch.mps.empty_cache()  # noqa
+#         delta = ChatDeltaSchema().dump({"content": content})
+#         choice = ChatCompletionChunkChoiceSchema().dump({"index": 0, "delta": delta, "finish_reason": None})
+#         yield sse(line=ChatCompletionChunkSchema().dump({"model": "baichuan2-7b-chat", "choices": [choice]}))  # noqa
+#         position = len(response)
+#     choice = ChatCompletionChunkChoiceSchema().dump({"index": 0, "delta": {}, "finish_reason": "stop"})
+#     yield sse(line=ChatCompletionChunkSchema().dump({"model": "baichuan2-7b-chat", "choices": [choice]}))  # noqa
+#     yield sse(line="[DONE]")
 
 
 # @blueprint.route("/completions", methods=["POST"])
 def create_chat_completion():
     """Chat接口"""
-    chat_dict = CreateChatCompletionSchema().load(request.json)
-    # if chat_dict["stream"]:
-    #     # 切换到流式
-    #     return current_app.response_class(response=stream_chat_generate(chat_dict["messages"]), mimetype="text/event-stream")
-    response = my_model.chat(my_tokenizer, chat_dict["messages"])
-    message = ChatMessageSchema().dump({"role": "assistant", "content": response})
-    choice = ChatCompletionChoiceSchema().dump({"index": 0, "message": message, "finish_reason": "stop"})
-    return ChatCompletionSchema().dump({"model": "baichuan2-7b-chat", "choices": [choice]})  # noqa
+    return 213
+    # chat_dict = CreateChatCompletionSchema().load(request.json)
+    # # if chat_dict["stream"]:
+    # #     # 切换到流式
+    # #     return current_app.response_class(response=stream_chat_generate(chat_dict["messages"]), mimetype="text/event-stream")
+    # response = my_model.chat(my_tokenizer, chat_dict["messages"])
+    # message = ChatMessageSchema().dump({"role": "assistant", "content": response})
+    # choice = ChatCompletionChoiceSchema().dump({"index": 0, "message": message, "finish_reason": "stop"})
+    # return ChatCompletionSchema().dump({"model": "baichuan2-7b-chat", "choices": [choice]})  # noqa
 
 
 if __name__ == "__main__":
