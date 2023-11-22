4b9d4daf463935b1b9bf606fa35c184d7a43c1eb
rhys2985 <rhys2985@163.com>
2023-11-22 14:46:33 +0800

    temp

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from json import dumps
-from os import system
+from os import system, getenv
 from time import time
 from typing import Dict, Tuple, Union
 from uuid import uuid4
 
+import gradio as gr
 import torch
+from fastapi import FastAPI
 from flasgger import Schema, fields
 from flask import Flask, Blueprint, Response, current_app, request, stream_with_context
 from flask_cors import CORS
@@ -72,8 +74,8 @@ class ChatCompletionSchema(Schema):
 
 
 def init_env() -> None:
-    # system("mkdir /tmp/dataset")
-    system("unzip /tmp/dataset/Baichuan2-7B-Chat.zip -d /tmp/dataset")
+    system("mkdir /tmp/dataset")
+    system("unzip /dataset/Baichuan2-7B-Chat.zip -d /tmp/dataset")
     system("chmod +x frpc/frpc")  # noqa
     system("nohup ./frpc/frpc -c frpc/frpc.ini &")  # noqa
     return
@@ -97,21 +99,21 @@ def init_model():
     return model, tokenizer
 
 
-def init_app() -> Tuple[Flask, Blueprint]:
-    """创建接口服务"""
-    app = Flask(__name__)  # 声明主服务
-    CORS(app=app)  # 允许跨域
-
-    app.register_blueprint(blueprint=blueprint)  # 注册蓝图
-
-    @app.after_request
-    def after_request(resp: Response) -> Response:
-        """请求后处理"""
-        if torch.backends.mps.is_available():  # noqa
-            torch.mps.empty_cache()  # noqa
-        return resp
-
-    return app, blueprint
+# def init_app() -> Tuple[Flask, Blueprint]:
+#     """创建接口服务"""
+#     app = Flask(__name__)  # 声明主服务
+#     CORS(app=app)  # 允许跨域
+# 
+#     app.register_blueprint(blueprint=blueprint)  # 注册蓝图
+# 
+#     @app.after_request
+#     def after_request(resp: Response) -> Response:
+#         """请求后处理"""
+#         if torch.backends.mps.is_available():  # noqa
+#             torch.mps.empty_cache()  # noqa
+#         return resp
+# 
+#     return app, blueprint
 
 
 init_env()
@@ -145,13 +147,13 @@ def stream_chat_generate(messages):
     yield sse(line="[DONE]")
 
 
-@blueprint.route("/completions", methods=["POST"])
+# @blueprint.route("/completions", methods=["POST"])
 def create_chat_completion():
     """Chat接口"""
     chat_dict = CreateChatCompletionSchema().load(request.json)
-    if chat_dict["stream"]:
-        # 切换到流式
-        return current_app.response_class(response=stream_chat_generate(chat_dict["messages"]), mimetype="text/event-stream")
+    # if chat_dict["stream"]:
+    #     # 切换到流式
+    #     return current_app.response_class(response=stream_chat_generate(chat_dict["messages"]), mimetype="text/event-stream")
     response = my_model.chat(my_tokenizer, chat_dict["messages"])
     message = ChatMessageSchema().dump({"role": "assistant", "content": response})
     choice = ChatCompletionChoiceSchema().dump({"index": 0, "message": message, "finish_reason": "stop"})
@@ -159,5 +161,14 @@ def create_chat_completion():
 
 
 if __name__ == "__main__":
-    my_app, _ = init_app()  # noqa
-    my_app.run(host="0.0.0.0", port=8262, debug=False)
+    # my_app, _ = init_app()  # noqa
+    # my_app.run(host="0.0.0.0", port=8262, debug=False)
+
+    app = FastAPI()
+    demo = gr.Interface(
+        fn=create_chat_completion,
+        inputs=gr.components.Textbox(label="Inputs"),
+        outputs=gr.components.Textbox(label="Outputs"),
+        allow_flagging="never"
+    )
+    app = gr.mount_gradio_app(app, demo, path=getenv("OPENI_GRADIO_URL"))  # noqa
