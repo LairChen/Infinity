7cf49ea73a8edaac4a0588225137cf28d166554a
rhys2985 <rhys2985@163.com>
2023-11-24 10:17:09 +0800

    更新 'app.py'

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from os import system, getenv
-from typing import List
+from typing import List, Tuple, Dict
 
 import gradio as gr
 import torch
 from fastapi import FastAPI
-from peft import AutoPeftModelForCausalLM
-from transformers import AutoTokenizer
+from peft import AutoPeftModelForCausalLM, PeftModelForCausalLM
+from transformers import AutoTokenizer, PreTrainedTokenizer
 from transformers.generation.utils import GenerationConfig
 
 
 def init_env() -> None:
+    """环境初始化"""
     system("mkdir /tmp/dataset")
     system("unzip /pretrainmodel/Baichuan2-7B-Chat.zip -d /tmp/dataset")
     return
 
 
-def init_model():
+def init_model() -> Tuple[PeftModelForCausalLM, PreTrainedTokenizer]:
+    """模型和词表初始化"""
     model = AutoPeftModelForCausalLM.from_pretrained(
         pretrained_model_name_or_path="/pretrainmodel",
         torch_dtype=torch.float16,
@@ -34,14 +36,15 @@ def init_model():
 
 
 def chat_with_model(history: List[str], content: str):  # noqa
-    """Chat接口"""
+    """模型流式输出"""
     for response in my_model.chat(my_tokenizer, [{"role": "user", "content": content}], stream=True):
         if torch.backends.mps.is_available():  # noqa
             torch.mps.empty_cache()  # noqa
         yield [(content, response)]
 
 
-def reset_user_input():
+def reset_user_input() -> Dict:
+    """清理用户输入空间"""
     return gr.update(value="")
 
 
@@ -49,7 +52,6 @@ def reset_user_input():
 init_env()
 my_model, my_tokenizer = init_model()
 app = FastAPI()
-
 with gr.Blocks(title="Infinity Model") as demo:
     gr.Markdown(value="<p align='center'><img src='https://openi.pcl.ac.cn/rhys2985/Infinity-llm/raw/branch/master/infinity.png' "
                       "style='height: 100px'/><p>")
@@ -59,9 +61,10 @@ with gr.Blocks(title="Infinity Model") as demo:
     chatbot = gr.Chatbot(label="Infinity Model", elem_classes="control-height")  # noqa
     textbox = gr.Textbox(lines=2, label="Input")
     with gr.Row():
-        submit_btn = gr.Button("Submit 🚀")
+        submit_btn = gr.Button("👉 Submit 👈")
     submit_btn.click(chat_with_model, [chatbot, textbox], [chatbot])
     submit_btn.click(reset_user_input, [], [textbox])
     gr.Markdown(value="<font size=4>⚠ I strongly advise you not to knowingly generate or spread harmful content, "
                       "including rumor, hatred, violence, reactionary, pornography, deception, etc. ⚠")
+demo.queue()
 app = gr.mount_gradio_app(app, demo, path=getenv("OPENI_GRADIO_URL"))  # noqa
