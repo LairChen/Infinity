d079cd9030db664e076de5338b5ad3b7732b8732
rhys <rhys2985@noreply.localhost>
2023-12-01 11:32:30 +0800

    动态

--- main.py
+++ main.py
@@ -1,51 +1,31 @@
-from argparse import ArgumentParser
-from os import system
+from json import load
+from os import system, listdir
+from re import match
 
 import torch
 from peft import AutoPeftModelForCausalLM
 from transformers import AutoTokenizer
 
-data_path = "/tmp/dataset"
-tune_path = "/tmp/output"
-parser = ArgumentParser()
-parser.add_argument("--model", "-m", help="base model name")
-args = parser.parse_args()
-model_name = args.model
+from utils import llm
 
-cmd_mkdir = "mkdir /tmp/dataset/tune"
-cmd_unzip = f"unzip {data_path}/{model_name}.zip -d {data_path}"  # noqa
-cmd_train = "deepspeed --hostfile='' core/train.py " \
-            "--report_to none " \
-            f"--model_name_or_path {data_path}/{model_name} " \
-            "--data_path data/train.json " \
-            "--output_dir /tmp/dataset/tune " \
-            "--model_max_length 1024 " \
-            "--num_train_epochs 20 " \
-            "--per_device_train_batch_size 1 " \
-            "--save_strategy steps " \
-            "--save_steps 80416 " \
-            "--save_total_limit 1 " \
-            "--lr_scheduler_type constant " \
-            "--learning_rate 2e-5 " \
-            "--adam_beta1 0.9 " \
-            "--adam_beta2 0.98 " \
-            "--adam_epsilon 1e-8 " \
-            "--max_grad_norm 1.0 " \
-            "--warmup_ratio 0.0 " \
-            "--weight_decay 1e-4 " \
-            "--logging_steps 1 " \
-            "--gradient_accumulation_steps 1 " \
-            "--gradient_checkpointing True " \
-            "--deepspeed data/deepspeed.json " \
-            "--bf16 True " \
-            "--tf32 False " \
-            "--use_lora True"
-cmd_predict = "python core/predict.py " \
-              f"--base {data_path}/{model_name} " \
-              f"--output /tmp/dataset/tune"
+
+def get_data_num() -> int:
+    """动态获取训练数据集规模"""
+    data = load(fp=open(file="data/train.json", encoding="utf-8"))
+    return len(data)
+
+
+def get_model_name() -> str:
+    """动态获取基座模型名称"""
+    for filename in listdir("/tmp/dataset"):
+        model = match(pattern="(.*)\.zip", string=filename)  # noqa
+        if model is not None:
+            return model.groups()[0]
+    raise FileNotFoundError("No existing base model")
 
 
 def merge_model_and_tokenizer() -> None:
+    """合并模型和词表以构建HF标准模型"""
     model = AutoPeftModelForCausalLM.from_pretrained(
         pretrained_model_name_or_path="/tmp/dataset/tune",
         torch_dtype=torch.float16,
@@ -63,9 +43,48 @@ def merge_model_and_tokenizer() -> None:
     return
 
 
+data_path = "/tmp/dataset"
+tune_path = "/tmp/output"
+# parser = ArgumentParser()
+# parser.add_argument("--model", "-m", help="base model name")
+# args = parser.parse_args()
+# model_name = get_model_name()
+
+# cmd_unzip = f"unzip {data_path}/{model_name}.zip -d {data_path}"  # noqa
+cmd_mkdir = "mkdir /tmp/dataset/tune"
+cmd_train = "deepspeed --hostfile='' core/train.py " \
+            "--report_to none " \
+            f"--model_name_or_path {data_path}/{get_model_name()} " \
+            "--data_path data/train.json " \
+            "--output_dir /tmp/dataset/tune " \
+            f"--model_max_length {llm['size']} " \
+            f"--num_train_epochs {llm['epoch']} " \
+            f"--per_device_train_batch_size {llm['batch']} " \
+            "--save_strategy steps " \
+            f"--save_steps {get_data_num() * llm['epoch'] // llm['batch']} " \
+            "--save_total_limit 1 " \
+            "--lr_scheduler_type constant " \
+            "--learning_rate 2e-5 " \
+            "--adam_beta1 0.9 " \
+            "--adam_beta2 0.98 " \
+            "--adam_epsilon 1e-8 " \
+            "--max_grad_norm 1.0 " \
+            "--warmup_ratio 0.0 " \
+            "--weight_decay 1e-4 " \
+            "--logging_steps 1 " \
+            "--gradient_accumulation_steps 1 " \
+            "--gradient_checkpointing True " \
+            "--deepspeed data/deepspeed.json " \
+            "--bf16 True " \
+            "--tf32 False " \
+            "--use_lora True"
+cmd_predict = "python core/predict.py " \
+              f"--base {data_path}/{get_model_name()} " \
+              f"--output /tmp/dataset/tune"
+
 if __name__ == "__main__":
-    system(cmd_mkdir)
-    system(cmd_unzip)  # 预训练模型解压缩
+    # system(cmd_unzip)  # 预训练模型解压缩
+    system(cmd_mkdir)  # 创建模型微调的临时路径
     system(cmd_train)  # 模型微调
     system(cmd_predict)  # 模型预测
-    merge_model_and_tokenizer()
+    merge_model_and_tokenizer()  # 合并微调参数和词表
