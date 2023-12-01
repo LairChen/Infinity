11c3ac3ae3df872f11bad2c327bf0a415ffb2adb
rhys <rhys2985@noreply.localhost>
2023-12-01 13:15:04 +0800

    add

--- utils/config.py
+++ utils/config.py
@@ -3,6 +3,14 @@ from configparser import ConfigParser
 cfg = ConfigParser()
 cfg.read(filenames="conf/config.ini", encoding="utf-8")
 
+appHost = cfg.get(section="app", option="app.host")
+appPort = cfg.getint(section="app", option="app.port")
+appAddr = cfg.get(section="app", option="app.addr")
+
+path_train_pretrain = cfg.get(section="path", option="path.train.pretrain")  # noqa
+path_train_finetune = cfg.get(section="path", option="path.train.finetune")  # noqa
+path_eval_finetune = cfg.get(section="path", option="path.eval.finetune")  # noqa
+
 llm = {
     "size": cfg.getint(section="model", option="model.max.length"),
     "epoch": cfg.getint(section="model", option="num.train.epochs"),
