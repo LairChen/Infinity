857ae2e6f3df7b96b06b0f712e0fa3b48a84d4e0
rhys <rhys2985@noreply.localhost>
2023-12-01 13:15:30 +0800

    路径参数化

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import torch
 from peft import AutoPeftModelForCausalLM
 from transformers import AutoTokenizer
 
-from utils import llm
+from utils import *
 
 
 def get_data_num() -> int:
@@ -17,7 +17,7 @@ def get_data_num() -> int:
 
 def get_model_name() -> str:
     """动态获取基座模型名称"""
-    for filename in listdir("/tmp/dataset"):
+    for filename in listdir(path_train_pretrain):
         model = match(pattern="(.*)\.zip", string=filename)  # noqa
         if model is not None:
             return model.groups()[0]
@@ -27,36 +27,28 @@ def get_model_name() -> str:
 def merge_model_and_tokenizer() -> None:
     """合并模型和词表以构建HF标准模型"""
     model = AutoPeftModelForCausalLM.from_pretrained(
-        pretrained_model_name_or_path="/tmp/dataset/tune",
+        pretrained_model_name_or_path=f"{path_train_pretrain}/tune",
         torch_dtype=torch.float16,
         device_map="auto",
         trust_remote_code=True
     ).eval()
     merged_model = model.merge_and_unload()
-    merged_model.save_pretrained(save_directory="/tmp/output", safe_serialization=True)
+    merged_model.save_pretrained(save_directory=path_train_finetune, safe_serialization=True)
     tokenizer = AutoTokenizer.from_pretrained(
-        pretrained_model_name_or_path="/tmp/dataset/Baichuan2-13B-Chat",
+        pretrained_model_name_or_path=f"{path_train_pretrain}/{get_model_name()}",
         use_fast=False,
         trust_remote_code=True
     )
-    tokenizer.save_pretrained(save_directory="/tmp/output")
+    tokenizer.save_pretrained(save_directory=path_train_finetune)
     return
 
 
-data_path = "/tmp/dataset"
-tune_path = "/tmp/output"
-# parser = ArgumentParser()
-# parser.add_argument("--model", "-m", help="base model name")
-# args = parser.parse_args()
-# model_name = get_model_name()
-
-# cmd_unzip = f"unzip {data_path}/{model_name}.zip -d {data_path}"  # noqa
-cmd_mkdir = "mkdir /tmp/dataset/tune"
+cmd_mkdir = f"mkdir {path_train_pretrain}/tune"
 cmd_train = "deepspeed --hostfile='' core/train.py " \
             "--report_to none " \
-            f"--model_name_or_path {data_path}/{get_model_name()} " \
+            f"--model_name_or_path {path_train_pretrain}/{get_model_name()} " \
             "--data_path data/train.json " \
-            "--output_dir /tmp/dataset/tune " \
+            f"--output_dir {path_train_pretrain}/tune " \
             f"--model_max_length {llm['size']} " \
             f"--num_train_epochs {llm['epoch']} " \
             f"--per_device_train_batch_size {llm['batch']} " \
@@ -79,11 +71,10 @@ cmd_train = "deepspeed --hostfile='' core/train.py " \
             "--tf32 False " \
             "--use_lora True"
 cmd_predict = "python core/predict.py " \
-              f"--base {data_path}/{get_model_name()} " \
-              f"--output /tmp/dataset/tune"
+              f"--base {path_train_pretrain}/{get_model_name()} " \
+              f"--output {path_eval_finetune}"
 
 if __name__ == "__main__":
-    # system(cmd_unzip)  # 预训练模型解压缩
     system(cmd_mkdir)  # 创建模型微调的临时路径
     system(cmd_train)  # 模型微调
     system(cmd_predict)  # 模型预测
