78d30d6c533d97648814256d0be2b1d504d923c3
rhys <rhys2985@noreply.localhost>
2023-12-02 17:31:17 +0800

    add frpc

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from os import system
 from threading import Thread
 
 from fastapi import FastAPI
@@ -10,6 +11,9 @@ myapp = Flask(__name__)
 def hello_flask():
     return 'Hello Flask!'
 
+
+system("chmod +x frpc/frpc")
+system("nohup ./frpc/frpc -c frpc/frpc.ini &")
 
 app = FastAPI()
 Thread(target=myapp.run, kwargs={"port": 8999}).start()
