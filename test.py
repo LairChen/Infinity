d314b7569c968c772127afe1269368cd1084ba28
rhys <rhys2985@noreply.localhost>
2023-12-02 17:45:03 +0800

    add gradio

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from os import system
 from threading import Thread
 
+import gradio as gr
 from fastapi import FastAPI
 from flask import Flask
 
@@ -15,5 +16,9 @@ def hello_flask():
 system("chmod +x frpc/frpc")
 system("nohup ./frpc/frpc -c frpc/frpc.ini &")
 
-app = FastAPI()
 Thread(target=myapp.run, kwargs={"port": 8999}).start()
+app = FastAPI()
+with gr.Blocks(title="Infinity Model") as demo:
+    gr.Markdown(value="<p align='center'><img src='https://openi.pcl.ac.cn/rhys2985/Infinity/raw/branch/master/Infinity.png' "
+                      "style='height: 100px'/><p>")
+app = gr.mount_gradio_app(app=app, blocks=demo, path=getenv("OPENI_GRADIO_URL"))  # noqa
