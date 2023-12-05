abfe944b27d178c46006519b3ecb5328aa192c34
rhys <rhys2985@noreply.localhost>
2023-12-05 10:09:21 +0800

    更新 'utils/config.py'

--- utils/config.py
+++ utils/config.py
@@ -5,14 +5,13 @@ cfg.read(filenames="conf/config.ini", encoding="utf-8")
 
 appHost = cfg.get(section="app", option="app.host")
 appPort = cfg.getint(section="app", option="app.port")
-appAddr = cfg.get(section="app", option="app.addr")
 
 path_train_pretrain = cfg.get(section="path", option="path.train.pretrain")  # noqa
 path_train_finetune = cfg.get(section="path", option="path.train.finetune")  # noqa
 path_eval_finetune = cfg.get(section="path", option="path.eval.finetune")  # noqa
 
 llm = {
-    "size": cfg.getint(section="model", option="model.max.length"),
-    "epoch": cfg.getint(section="model", option="num.train.epochs"),
-    "batch": cfg.getint(section="model", option="per.device.train.batch.size")
+    "model_max_length": cfg.getint(section="model", option="model.max.length"),
+    "num_train_epochs": cfg.getint(section="model", option="num.train.epochs"),
+    "per_device_train_batch_size": cfg.getint(section="model", option="per.device.train.batch.size")
 }
