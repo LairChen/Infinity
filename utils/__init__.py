3af1fa36f459bfaf87489ac26425c1631bd76295
rhys <rhys2985@noreply.localhost>
2023-12-08 10:46:49 +0800

    更新 'utils/__init__.py'

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
+from .schema import *
 from .config import llm, appHost, appPort, path_train_pretrain, path_train_finetune, path_eval_finetune
 
 __all__ = [s for s in dir() if not s.startswith("_")]
