1de13b74e3818bb49bdeea764dc869ff1d7eff1f
rhys <rhys2985@noreply.localhost>
2023-12-14 10:41:55 +0800

    更新 'utils/__init__.py'

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,12 @@
-from .schema import *
 from .config import *
+from .model import *
+from .schema import *
+
+model_type_dict = {
+    "Baichuan2-7B-Chat": BaichuanModel,  # noqa
+    "Baichuan2-13B-Chat": BaichuanModel,  # noqa
+    "deepseek-coder-6.7b-instruct": DeepseekModel,  # noqa
+    "m3e-large": M3eModel
+}
 
 __all__ = [s for s in dir() if not s.startswith("_")]
