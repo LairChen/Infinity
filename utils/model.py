984a2a8bf9db9c7cf578b40dd264be1b73985a6c
rhys <rhys2985@noreply.localhost>
2023-12-14 16:07:13 +0800

    新增百川

--- utils/model.py
+++ utils/model.py
@@ -1,11 +1,19 @@
 from abc import ABC
+from typing import Dict, List
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+# 模型常量
+CONTENT_MAX_LENGTH = 4096
+EMBEDDING_MAX_LENGTH = 1536
 
 
 class BaseModel(object):
-    """所有模型的基类"""
+    """base class for all models"""
 
     def __init__(self):
-        self.name = ""
+        self.name = "Infinity"
         self.model = None
         self.tokenizer = None
 
@@ -17,25 +25,51 @@ class BaseModel(object):
         """生成模型答复文本"""
         raise NotImplementedError("method: generate")
 
-    def stream(self):
+    def stream(self, conversation: List[Dict[str, str]]):
         """流式生成模型答复，使用para模式"""
         raise NotImplementedError("method: stream")
 
-    def embedding(self):
+    def embedding(self, content: str):
         """生成模型嵌入结果"""
         raise NotImplementedError("method: embedding")
 
 
 class BaichuanModel(BaseModel, ABC):  # noqa
-    def __init__(self):
+    """class for baichuan"""
+
+    def __init__(self, name: str, path: str):
         super().__init__()
+        self.name = name
+        self.model = AutoModelForCausalLM.from_pretrained(
+            pretrained_model_name_or_path=path,
+            torch_dtype=torch.float16,
+            device_map="auto",
+            trust_remote_code=True
+        ).eval()
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            pretrained_model_name_or_path=path,
+            use_fast=False,
+            trust_remote_code=True
+        )
+
+    def stream(self, conversation: List[Dict[str, str]]):
+        for answer in self.model.chat(self.tokenizer, conversation, stream=True):
+            if torch.backends.mps.is_available():  # noqa
+                torch.mps.empty_cache()  # noqa
+            if len(answer) >= CONTENT_MAX_LENGTH:
+                break
+            yield answer
 
 
 class DeepseekModel(BaseModel, ABC):  # noqa
+    """class for deepseek"""
+
     def __init__(self):
         super().__init__()
 
 
 class M3eModel(BaseModel, ABC):
+    """class for m3e"""
+
     def __init__(self):
         super().__init__()
