adbb61ac80f224b4f9f8ef9dcaee218635c8fd11
rhys <rhys2985@noreply.localhost>
2023-12-19 17:23:22 +0800

    更新 'utils/model.py'

--- utils/model.py
+++ utils/model.py
@@ -1,4 +1,3 @@
-from abc import ABC
 from threading import Thread
 from typing import Dict, List
 
@@ -8,16 +7,19 @@ from sentence_transformers import SentenceTransformer
 from sklearn.preprocessing import PolynomialFeatures
 from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
 
-# 模型常量
-MAX_CONTENT_LENGTH = 4096
-MAX_EMBEDDING_LENGTH = 1536  # OpenAI API 嵌入维度标准 1536
-
 
 class BaseModel(object):
     """base class for all models"""
 
-    def __init__(self):
-        self.name = "Infinity"
+    def __init__(self, name: str):
+        self.name = name
+
+
+class BaseChatModel(BaseModel):
+    """base class for chat models"""
+
+    def __init__(self, name: str):
+        super(BaseChatModel, self).__init__(name=name)
         self.model = None
         self.tokenizer = None
 
@@ -30,20 +32,27 @@ class BaseModel(object):
         raise NotImplementedError("method: generate")
 
     def stream(self, conversation: List[Dict[str, str]]):
-        """流式生成模型答复，使用para模式"""
+        """流式生成模型答复，使用字符模式"""
         raise NotImplementedError("method: stream")
+
+
+class BaseEmbeddingsModel(BaseModel):
+    """base class for embeddings models"""
+
+    def __init__(self, name: str):
+        super(BaseEmbeddingsModel, self).__init__(name=name)
+        self.model = None
 
     def embedding(self, sentence: List[str]) -> List[List[float]]:
         """生成模型嵌入结果"""
         raise NotImplementedError("method: embedding")
 
 
-class BaichuanModel(BaseModel, ABC):  # noqa
-    """class for baichuan"""
+class BaichuanModel(BaseChatModel):  # noqa
+    """class for baichuan model"""
 
     def __init__(self, name: str, path: str):
-        super().__init__()
-        self.name = name
+        super(BaichuanModel, self).__init__(name=name)
         self.model = AutoModelForCausalLM.from_pretrained(
             pretrained_model_name_or_path=path,
             torch_dtype=torch.float16,
@@ -60,20 +69,19 @@ class BaichuanModel(BaseModel, ABC):  # noqa
         return self.model.chat(self.tokenizer, conversation)
 
     def stream(self, conversation: List[Dict[str, str]]):
+        position = 0
         for answer in self.model.chat(self.tokenizer, conversation, stream=True):
             if torch.backends.mps.is_available():  # noqa
                 torch.mps.empty_cache()  # noqa
-            if len(answer) >= MAX_CONTENT_LENGTH:
-                break
-            yield answer
+            yield answer[position:]
+            position = len(answer)
 
 
-class DeepseekModel(BaseModel, ABC):  # noqa
-    """class for deepseek"""
+class DeepseekModel(BaseChatModel):  # noqa
+    """class for deepseek model"""
 
     def __init__(self, name: str, path: str):
-        super().__init__()
-        self.name = name
+        super(DeepseekModel, self).__init__(name=name)
         self.model = AutoModelForCausalLM.from_pretrained(
             pretrained_model_name_or_path=path,
             torch_dtype=torch.bfloat16,
@@ -84,6 +92,12 @@ class DeepseekModel(BaseModel, ABC):  # noqa
             use_fast=False,
             trust_remote_code=True
         )
+
+    def generate(self, conversation: List[Dict[str, str]]) -> str:
+        input_ids = self.tokenizer.apply_chat_template(conversation, return_tensors="pt").to(self.model.device)
+        output_ids = self.model.generate(inputs=input_ids, do_sample=False, max_new_tokens=1024,
+                                         num_beans=1, repetition_penalty=1, eos_token_id=32021)
+        return self.tokenizer.decode(token_ids=output_ids[0][len(input_ids[0]):], skip_special_tokens=True)
 
     def stream(self, conversation: List[Dict[str, str]]):
         input_ids = self.tokenizer.apply_chat_template(conversation, return_tensors="pt").to(self.model.device)
@@ -98,22 +112,17 @@ class DeepseekModel(BaseModel, ABC):  # noqa
             "eos_token_id": 32021
         }
         Thread(target=self.model.generate, kwargs=generate_kwargs).start()
-        answer = ""
         for text in streamer:
-            answer += text.replace("<|EOT|>", "")
             if torch.backends.mps.is_available():  # noqa
                 torch.mps.empty_cache()  # noqa
-            if len(answer) >= MAX_CONTENT_LENGTH:
-                break
-            yield answer
+            yield text.replace("<|EOT|>", "")
 
 
-class DeepseekGPTQModel(BaseModel, ABC):  # noqa
-    """class for deepseek GPTQ"""
+class DeepseekGPTQModel(BaseChatModel):  # noqa
+    """class for deepseek GPTQ model"""
 
     def __init__(self, name: str, path: str):
-        super().__init__()
-        self.name = name
+        super(DeepseekGPTQModel, self).__init__(name=name)
         self.model = AutoModelForCausalLM.from_pretrained(
             pretrained_model_name_or_path=path,
             torch_dtype=torch.float16,
@@ -125,16 +134,24 @@ class DeepseekGPTQModel(BaseModel, ABC):  # noqa
             use_fast=False,
             trust_remote_code=True
         )
+        self.tmpl = "You are an AI programming assistant, utilizing the Deepseek Coder model, developed by Deepseek Company, " \
+                    "and you only answer questions related to computer science. " \
+                    "For politically sensitive questions, security and privacy issues, and other non-computer science questions, " \
+                    "you will refuse to answer.\n" \
+                    "### Instruction:\n" \
+                    "{}\n" \
+                    "### Response:\n"
+
+    def generate(self, conversation: List[Dict[str, str]]) -> str:
+        input_ids = self.tokenizer(
+            self.tmpl.format(conversation[-1]['content']), return_tensors="pt").input_ids.to(self.model.device)
+        output_ids = self.model.generate(inputs=input_ids, do_sample=False, max_new_tokens=1024,
+                                         num_beans=1, repetition_penalty=1, eos_token_id=32021)
+        return self.tokenizer.decode(token_ids=output_ids[0], skip_special_tokens=True)
 
     def stream(self, conversation: List[Dict[str, str]]):
-        template = "You are an AI programming assistant, utilizing the Deepseek Coder model, developed by Deepseek Company, " \
-                   "and you only answer questions related to computer science. " \
-                   "For politically sensitive questions, security and privacy issues, and other non-computer science questions, " \
-                   "you will refuse to answer.\n" \
-                   "### Instruction:\n" \
-                   f"{conversation[-1]['content']}\n" \
-                   "### Response:\n"
-        input_ids = self.tokenizer(template, return_tensors="pt").input_ids.to(self.model.device)
+        input_ids = self.tokenizer(
+            self.tmpl.format(conversation[-1]['content']), return_tensors="pt").input_ids.to(self.model.device)
         streamer = TextIteratorStreamer(tokenizer=self.tokenizer, timeout=10.0, skip_prompt=True, skip_special_tokens=True)
         generate_kwargs = {
             "input_ids": input_ids,
@@ -146,33 +163,29 @@ class DeepseekGPTQModel(BaseModel, ABC):  # noqa
             "eos_token_id": 32021
         }
         Thread(target=self.model.generate, kwargs=generate_kwargs).start()
-        answer = ""
         for text in streamer:
-            answer += text.replace("<|EOT|>", "")
             if torch.backends.mps.is_available():  # noqa
                 torch.mps.empty_cache()  # noqa
-            if len(answer) >= MAX_CONTENT_LENGTH:
-                break
-            yield answer
+            yield text.replace("<|EOT|>", "")
 
 
-class M3eModel(BaseModel, ABC):
-    """class for m3e"""
+class M3eModel(BaseEmbeddingsModel):
+    """class for m3e model"""
 
     def __init__(self, name: str, path: str):
-        super().__init__()
-        self.name = name
+        super(M3eModel, self).__init__(name=name)
         self.model = SentenceTransformer(model_name_or_path=path)
 
     def embedding(self, sentence: str) -> List[float]:
         result = self.model.encode(sentences=sentence)
-        if len(result) < MAX_EMBEDDING_LENGTH:
+        # OpenAI API 嵌入维度标准 1536
+        if len(result) < 1536:
             result = PolynomialFeatures(degree=2).fit_transform(X=result.reshape(1, -1)).flatten()
-            if len(result) < MAX_EMBEDDING_LENGTH:
-                result = np.pad(array=result, pad_width=(0, MAX_EMBEDDING_LENGTH - len(result)))
+            if len(result) < 1536:
+                result = np.pad(array=result, pad_width=(0, 1536 - len(result)))
             else:
-                result = result[:MAX_EMBEDDING_LENGTH]
+                result = result[:1536]
         else:
-            result = result[:MAX_EMBEDDING_LENGTH]
+            result = result[:1536]
         result = result / np.linalg.norm(x=result)
         return result.tolist()
