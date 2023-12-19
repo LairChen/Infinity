bccebfc65f4593e16233c6e572eb4fc53561d250
rhys <rhys2985@noreply.localhost>
2023-12-19 17:23:47 +0800

    分类

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,13 +2,16 @@ from .config import *
 from .model import *
 from .schema import *
 
-MODEL_TYPE_DICT = {
+CHAT_MODEL_TYPE = {
     "Baichuan2-7B-Chat": BaichuanModel,  # noqa
     "Baichuan2-13B-Chat": BaichuanModel,  # noqa
     "deepseek-coder-1.3b-instruct": DeepseekModel,  # noqa
     "deepseek-coder-6.7b-instruct": DeepseekModel,  # noqa
     "deepseek-coder-33b-instruct": DeepseekModel,  # noqa
-    "deepseek-coder-33B-instruct-GPTQ": DeepseekGPTQModel,  # noqa
+    "deepseek-coder-33B-instruct-GPTQ": DeepseekGPTQModel  # noqa
+}
+
+EMBEDDINGS_MODEL_TYPE = {
     "m3e-base": M3eModel,
     "m3e-small": M3eModel,
     "m3e-large": M3eModel
