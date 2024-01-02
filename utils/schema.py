e28219e22ed903118b7269560e31303d9c7a40c0
rhys <rhys2985@noreply.localhost>
2024-01-02 11:28:03 +0800

    新增chat非流式

--- utils/schema.py
+++ utils/schema.py
@@ -7,6 +7,8 @@ from marshmallow import validate
 
 # 使用marshmallow做序列化和参数校验
 
+# Chat接口
+
 class ChatMessageSchema(Schema):
     """Chat消息结构映射"""
     role = fields.Str(required=True)
@@ -17,7 +19,7 @@ class ChatRequestSchema(Schema):
     """Chat接口请求数据结构解析"""
     model = fields.Str(required=True)
     messages = fields.List(fields.Nested(nested=ChatMessageSchema), required=True)  # noqa
-    stream = fields.Bool(load_default=True)
+    stream = fields.Bool(required=True)
     max_tokens = fields.Int(load_default=1024)
     n = fields.Int(load_default=1)
     seed = fields.Int(load_default=1)
@@ -27,16 +29,17 @@ class ChatRequestSchema(Schema):
     frequency_penalty = fields.Float(load_default=0.0)
 
 
-class ChatDeltaSchema(Schema):
-    """Chat流式结构映射"""
-    role = fields.Str(required=True)
-    content = fields.Str(required=True)
-
-
 class ChatChoiceSchema(Schema):
+    """Chat消息选择器"""
+    index = fields.Int(required=True)
+    message = fields.Nested(nested=ChatMessageSchema)  # noqa
+    finish_reason = fields.Str(validate=validate.OneOf(["stop", "length", "content_filter", "function_call"]))  # noqa
+
+
+class ChatChoiceChunkSchema(Schema):
     """Chat流式消息选择器"""
     index = fields.Int(required=True)
-    delta = fields.Nested(nested=ChatDeltaSchema)  # noqa
+    delta = fields.Nested(nested=ChatMessageSchema)  # noqa
     finish_reason = fields.Str(validate=validate.OneOf(["stop", "length", "content_filter", "function_call"]))  # noqa
 
 
@@ -44,10 +47,52 @@ class ChatResponseSchema(Schema):
     """Chat接口响应数据结构映射"""
     id = fields.Str(dump_default=lambda: uuid4().hex)
     created = fields.Int(dump_default=lambda: int(time()))
-    model = fields.Str(required=True)  # noqa
+    model = fields.Str(required=True)
     choices = fields.List(fields.Nested(nested=ChatChoiceSchema), required=True)  # noqa
-    object = fields.Constant(constant="chat.completions")
+    object = fields.Constant(constant="chat")
 
+
+class ChatResponseChunkSchema(Schema):
+    """Chat接口流式响应数据结构映射"""
+    id = fields.Str(dump_default=lambda: uuid4().hex)
+    created = fields.Int(dump_default=lambda: int(time()))
+    model = fields.Str(required=True)
+    choices = fields.List(fields.Nested(nested=ChatChoiceChunkSchema), required=True)  # noqa
+    object = fields.Constant(constant="chat")
+
+
+# # Completions接口
+#
+# class CompletionsRequestSchema(Schema):
+#     model = fields.Str(required=True)
+#     prompt = fields.Raw(metadata={"example": "Say this is a test"})
+#     stream = fields.Bool(load_default=False)
+#     max_tokens = fields.Int(load_default=1024)
+#     n = fields.Int(load_default=1)
+#     seed = fields.Int(load_default=1)
+#     top_p = fields.Float(load_default=1.0)
+#     temperature = fields.Float(load_default=1.0)
+#     presence_penalty = fields.Float(load_default=0.0)
+#     frequency_penalty = fields.Float(load_default=0.0)
+#     logit_bias = fields.Dict(load_default=None)  # noqa
+#
+#
+# class CompletionChoiceSchema(Schema):
+#     index = fields.Int(required=True)
+#     text = fields.Str(required=True)
+#     logprobs = fields.Dict(load_default=None)  # noqa
+#     finish_reason = fields.Str(validate=validate.OneOf(["stop", "length", "content_filter", "function_call"]))  # noqa
+#
+#
+# class CompletionsResponseSchema(Schema):
+#     id = fields.Str(dump_default=lambda: uuid4().hex)
+#     created = fields.Int(dump_default=lambda: int(time()))
+#     model = fields.Str(required=True)
+#     choices = fields.List(fields.Nested(nested=CompletionChoiceSchema), required=True)  # noqa
+#     object = fields.Constant(constant="completions")
+
+
+# Embeddings接口
 
 class EmbeddingsRequestSchema(Schema):
     """Embeddings接口请求数据结构解析"""
