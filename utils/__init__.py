bd2ecddd673a4a301c7549d26466a7565d1a083a
rhys2985 <rhys2985@noreply.localhost>
2024-02-01 15:04:49 +0800

    初始化脚本迁移

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,7 @@
+from os import listdir
+from re import match
+from typing import Union, Optional
+
 from .config import appHost, appPort, path_train_pretrain, path_train_finetune, path_eval_pretrain, path_eval_finetune, llm
 from .model import Baichuan2ChatModel, CodefuseDeepseekModel, DeepseekCoderInstructModel, Internlm2ChatModel, SusChatModel
 from .model import BceModel, M3eModel
@@ -31,5 +35,32 @@ EMBEDDING_MODEL_TYPE = {
     "m3e-small": M3eModel,
     "m3e-large": M3eModel
 }
+
+
+def init_language_model() -> Union[ChatModel, CompletionModel]:
+    """初始化对话/补全模型"""
+    with open(file="{}/model_type.txt".format(path_eval_finetune), mode="r", encoding="utf-8") as f:
+        my_model_name = f.read().strip()
+    if CHAT_MODEL_TYPE.get(my_model_name, None) is not None:
+        my_model = CHAT_MODEL_TYPE[my_model_name](name=my_model_name, path=path_eval_finetune)
+    elif COMPLETION_MODEL_TYPE.get(my_model_name, None) is not None:
+        my_model = COMPLETION_MODEL_TYPE[my_model_name](name=my_model_name, path=path_eval_finetune)
+    else:
+        raise FileNotFoundError("no existing language model")
+    return my_model
+
+
+def init_embedding_model() -> Optional[EmbeddingModel]:
+    """初始化嵌入模型"""
+    for filename in listdir(path_eval_pretrain):
+        modelname = match(pattern="(.*)\.zip", string=filename)  # noqa
+        if modelname is not None:
+            my_model_name = modelname.groups()[0]
+            break
+    else:
+        return None
+    my_model = EMBEDDING_MODEL_TYPE[my_model_name](name=my_model_name, path=my_model_name)
+    return my_model
+
 
 __all__ = [s for s in dir() if not s.startswith("_")]
